ed6eb4b77b7342b61853980b5d468677075bc4a7
Larry Liang <lian999111@gmail.com>
2022-05-16 09:23:53 +0200

    Remove unused arguent

--- model.py
+++ model.py
@@ -5,14 +5,13 @@ import torch.nn.functional as F
 class QNetwork(nn.Module):
     """Actor (Policy) Model."""
 
-    def __init__(self, state_size, action_size, seed, layer_unit_sizes):
+    def __init__(self, state_size, action_size, seed):
         """Initialize parameters and build model.
         Params
         ======
             state_size (int): Dimension of each state
             action_size (int): Dimension of each action
             seed (int): Random seed
-            layer_unit_sizes (list): Sizes for perceptron's layers 
         """
         super(QNetwork, self).__init__()
         self.seed = torch.manual_seed(seed)
